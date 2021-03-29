14d8f26b49e07125d5b638af31a798f24b3bfdca
Usman <usman@jfreaks.com>
2021-03-29 13:38:09 +0500

    Restart the application to update database on manual update request

--- src/main/groovy/io/ipgeolocation/databaseReader/controllers/DatabaseController.groovy
+++ src/main/groovy/io/ipgeolocation/databaseReader/controllers/DatabaseController.groovy
@@ -2,8 +2,7 @@ package io.ipgeolocation.databaseReader.controllers
 
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
-import io.ipgeolocation.databaseReader.services.database.DatabaseUpdateService
-import org.springframework.beans.factory.annotation.Autowired
+import io.ipgeolocation.databaseReader.IpgeolocationDatabaseReaderApplication
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PostMapping
 
@@ -11,15 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping
 @Slf4j
 @Controller
 class DatabaseController {
-    private final DatabaseUpdateService databaseUpdateService
-
-    @Autowired
-    DatabaseController(DatabaseUpdateService databaseUpdateService) {
-        this.databaseUpdateService = databaseUpdateService
-    }
 
     @PostMapping(path = "/database/update", produces = "application/jsob")
     def updateDatabase() {
-        databaseUpdateService.fetchAndLoadDatabaseIfUpdated()
+        IpgeolocationDatabaseReaderApplication.restart()
     }
 }
