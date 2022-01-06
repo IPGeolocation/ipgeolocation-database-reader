abcdf5f2c41b0a9e4f098149551fdedfb244d75c
Usman Liaqat <usmanbinliaqat>
2022-01-06 21:34:55 +0500

    Lookup and parse ip-geolocation response from MMDB

--- src/main/groovy/io/ipgeolocation/databaseReader/services/database/MMDBDatabaseService.groovy
+++ src/main/groovy/io/ipgeolocation/databaseReader/services/database/MMDBDatabaseService.groovy
@@ -2,6 +2,8 @@ package io.ipgeolocation.databaseReader.services.database
 
 import com.google.common.net.InetAddresses
 import com.maxmind.db.CHMCache
+import com.maxmind.db.MaxMindDbConstructor
+import com.maxmind.db.MaxMindDbParameter
 import com.maxmind.db.Reader
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
@@ -83,8 +85,13 @@ class MMDBDatabaseService implements DatabaseService {
     }
 
     @Override
-    IPGeolocation findIPGeolocation(InetAddress inetAddress) {
-        ipGeolocationMMDBReader.get(inetAddress, IPGeolocation.class)
+    IPGeolocation findIPGeolocation(String ipAddress) {
+        return null
+    }
+
+    @Override
+    IPGeolocationMMDB findIPGeolocation(InetAddress inetAddress) {
+        ipGeolocationMMDBReader.get(inetAddress, IPGeolocationMMDB.class)
     }
 
     @Override
@@ -95,5 +102,131 @@ class MMDBDatabaseService implements DatabaseService {
     @Override
     Boolean isCloudProvider(String name) {
         cloudProviderIndexer.isCloudProvider(name)
+    }
+}
+
+@CompileStatic
+class IPGeolocationMMDB {
+    final String continentCode
+    final MultiLangValueMMDB continentName
+    final String countryCodeISO2
+    final String countryCodeISO3
+    final MultiLangValueMMDB countryName
+    final MultiLangValueMMDB countryCapital
+    final MultiLangValueMMDB stateProvince
+    final MultiLangValueMMDB district
+    final MultiLangValueMMDB city
+    final String zipCode
+    final float latitude
+    final float longitude
+    final String geoNameId
+    final String timeZone
+    final String isp
+    final String connectionType
+    final String organization
+    final BigInteger asNumber
+    final CurrencyMMDB currency
+    final String callingCode
+    final String languages
+    final String tld
+
+    @MaxMindDbConstructor
+    IPGeolocationMMDB(
+            @MaxMindDbParameter(name = "continent_code") String continentCode,
+            @MaxMindDbParameter(name = "continent_name") MultiLangValueMMDB continentName,
+            @MaxMindDbParameter(name = "country_code2") String countryCodeISO2,
+            @MaxMindDbParameter(name = "country_code3") String countryCodeISO3,
+            @MaxMindDbParameter(name = "country_name") MultiLangValueMMDB countryName,
+            @MaxMindDbParameter(name = "country_capital") MultiLangValueMMDB countryCapital,
+            @MaxMindDbParameter(name = "state_prov") MultiLangValueMMDB stateProvince,
+            @MaxMindDbParameter(name = "district") MultiLangValueMMDB district,
+            @MaxMindDbParameter(name = "city") MultiLangValueMMDB city,
+            @MaxMindDbParameter(name = "zip_code") String zipCode,
+            @MaxMindDbParameter(name = "latitude") float latitude,
+            @MaxMindDbParameter(name = "longitude") float longitude,
+            @MaxMindDbParameter(name = "geoname_id") String geoNameId,
+            @MaxMindDbParameter(name = "time_zone") String timeZone,
+            @MaxMindDbParameter(name = "isp") String isp,
+            @MaxMindDbParameter(name = "connection_type") String connectionType,
+            @MaxMindDbParameter(name = "organization") String organization,
+            @MaxMindDbParameter(name = "as_number") BigInteger asNumber,
+            @MaxMindDbParameter(name = "currency") CurrencyMMDB currency,
+            @MaxMindDbParameter(name = "calling_code") String callingCode,
+            @MaxMindDbParameter(name = "languages") String languages,
+            @MaxMindDbParameter(name = "tld") String tld) {
+        this.continentCode = continentCode
+        this.continentName = continentName
+        this.countryCodeISO2 = countryCodeISO2
+        this.countryCodeISO3 = countryCodeISO3
+        this.countryName = countryName
+        this.countryCapital = countryCapital
+        this.stateProvince = stateProvince
+        this.district = district
+        this.city = city
+        this.zipCode = zipCode
+        this.latitude = latitude
+        this.longitude = longitude
+        this.geoNameId = geoNameId
+        this.timeZone = timeZone
+        this.isp = isp
+        this.connectionType = connectionType
+        this.organization = organization
+        this.asNumber = asNumber
+        this.currency = currency
+        this.callingCode = callingCode
+        this.languages = languages
+        this.tld = tld
+    }
+}
+
+@CompileStatic
+class MultiLangValueMMDB {
+    final String en
+    final String de
+    final String cs
+    final String es
+    final String fr
+    final String it
+    final String ja
+    final String ru
+    final String zh
+
+    @MaxMindDbConstructor
+    MultiLangValueMMDB(
+            @MaxMindDbParameter(name = "en") String en,
+            @MaxMindDbParameter(name = "de") String de,
+            @MaxMindDbParameter(name = "cs") String cs,
+            @MaxMindDbParameter(name = "es") String es,
+            @MaxMindDbParameter(name = "fr") String fr,
+            @MaxMindDbParameter(name = "it") String it,
+            @MaxMindDbParameter(name = "ja") String ja,
+            @MaxMindDbParameter(name = "ru") String ru,
+            @MaxMindDbParameter(name = "zh") String zh) {
+        this.en = en
+        this.de = de
+        this.cs = cs
+        this.es = es
+        this.fr = fr
+        this.it = it
+        this.ja = ja
+        this.ru = ru
+        this.zh = zh
+    }
+}
+
+@CompileStatic
+class CurrencyMMDB {
+    final String code
+    final String name
+    final String symbol
+
+    @MaxMindDbConstructor
+    CurrencyMMDB(
+            @MaxMindDbParameter(name = "code") String code,
+            @MaxMindDbParameter(name = "name") String name,
+            @MaxMindDbParameter(name = "symbol") String symbol) {
+        this.code = code
+        this.symbol = symbol
+        this.name = name
     }
 }
