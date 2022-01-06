e4842c3b3bfae535dc2caba6e4c294ec43d3e6b1
Usman Liaqat <usmanbinliaqat>
2022-01-06 13:48:13 +0500

    Removed the toCSV method. Using @ToString annotation to generate a toString method in the domain class. Changed Boolean type variables to String type variables. Class fields have been made private final.

--- src/main/groovy/io/ipgeolocation/databaseReader/databases/ipsecurity/IPSecurity.groovy
+++ src/main/groovy/io/ipgeolocation/databaseReader/databases/ipsecurity/IPSecurity.groovy
@@ -1,15 +1,49 @@
 package io.ipgeolocation.databaseReader.databases.ipsecurity
 
+import com.maxmind.db.MaxMindDbConstructor
+import com.maxmind.db.MaxMindDbParameter
 import groovy.transform.CompileStatic
+import groovy.transform.ToString
 
 @CompileStatic
+@ToString(includeFields = true)
 class IPSecurity {
-    String ipAddress
-    Integer threatScore
-    String proxyType
-    Boolean isTor, isProxy, isAnonymous, isKnownAttacker, isBot, isSpam
+    private final String ipAddress
+    private final Integer threatScore
+    private final String proxyType, isTor, isProxy, isAnonymous, isKnownAttacker, isBot, isSpam
 
-    final String toCSV() {
-        "${ipAddress},${threatScore},${isTor},${isProxy},${proxyType},${isAnonymous},${isKnownAttacker}".toString()
+    IPSecurity(String ipAddress, Integer threatScore, String isProxy, String proxyType, String isTor,
+               String isAnonymous, String isKnownAttacker, String isBot, String isSpam) {
+        this.ipAddress = ipAddress
+        this.threatScore = threatScore
+        this.isProxy = isProxy
+        this.proxyType = proxyType
+        this.isTor = isTor
+        this.isAnonymous = isAnonymous
+        this.isKnownAttacker = isKnownAttacker
+        this.isBot = isBot
+        this.isSpam = isSpam
+    }
+
+    @MaxMindDbConstructor
+    IPSecurity(
+            @MaxMindDbParameter(name = "threat_score") Integer threatScore,
+            @MaxMindDbParameter(name = "proxy_type") String proxyType,
+            @MaxMindDbParameter(name = "is_tor") String isTor,
+            @MaxMindDbParameter(name = "is_proxy") String isProxy,
+            @MaxMindDbParameter(name = "is_anonymous") String isAnonymous,
+            @MaxMindDbParameter(name = "is_known_attacker") String isKnownAttacker,
+            @MaxMindDbParameter(name = "is_bot") String isBot,
+            @MaxMindDbParameter(name = "isSpam") String isSpam
+    ) {
+        this.ipAddress = null
+        this.threatScore = threatScore
+        this.proxyType = proxyType
+        this.isTor = isTor
+        this.isProxy = isProxy
+        this.isAnonymous = isAnonymous
+        this.isKnownAttacker = isKnownAttacker
+        this.isBot = isBot
+        this.isSpam = isSpam
     }
 }
