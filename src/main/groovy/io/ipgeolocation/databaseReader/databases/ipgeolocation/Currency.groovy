66d6bc38bb07e484f93e50456f2934616fca85e9
Usman Liaqat <usmanbinliaqat>
2022-01-07 13:19:56 +0500

    Set component qualifier. And IPGeolocation lookup response type set to IPGeolocation.

---
+++ src/main/groovy/io/ipgeolocation/databaseReader/databases/ipgeolocation/Currency.groovy
@@ -0,0 +1,22 @@
+package io.ipgeolocation.databaseReader.databases.mmdb
+
+import com.maxmind.db.MaxMindDbConstructor
+import com.maxmind.db.MaxMindDbParameter
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class Currency {
+    final String code
+    final String name
+    final String symbol
+
+    @MaxMindDbConstructor
+    Currency(
+            @MaxMindDbParameter(name = "code") String code,
+            @MaxMindDbParameter(name = "name") String name,
+            @MaxMindDbParameter(name = "symbol") String symbol) {
+        this.code = code
+        this.symbol = symbol
+        this.name = name
+    }
+}
--- src/main/groovy/io/ipgeolocation/databaseReader/databases/mmdb/Currency.groovy
+++
@@ -1,4 +0,0 @@
-package io.ipgeolocation.databaseReader.databases.mmdb
-
-class Currency {
-}
